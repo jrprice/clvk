34676d831b6df63fcba1502e650a4ff339748f86
James Price <jrprice@google.com>
2019-12-02 16:41:05 -0500

    Check formatting via Travis CI

--- tests/travis/step-install.sh
+++ tests/travis/step-install.sh
@@ -3,7 +3,10 @@
 set -ex
 
 if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
-    sudo apt-get install -qq g++-7
+    sudo add-apt-repository -y 'deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial main'
+    wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+    sudo apt-get update
+    sudo apt-get install -qq g++-7 clang-format
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
 fi
 
