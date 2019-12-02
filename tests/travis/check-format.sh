3245d1a3b6276a8e58147cd891b0fa944b77c23e
James Price <jrprice@google.com>
2019-12-02 16:41:05 -0500

    Check formatting via Travis CI

---
+++ tests/travis/check-format.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Run git-clang-format to check for violations
+CLANG_FORMAT_OUTPUT=/tmp/clvk-clang-format-output.txt
+git-clang-format --diff origin/master --extensions cpp,hpp >$CLANG_FORMAT_OUTPUT
+
+# Check for no-ops
+grep '^no modified files to format$' "$CLANG_FORMAT_OUTPUT" && exit 0
+grep '^clang-format did not modify any files$' "$CLANG_FORMAT_OUTPUT" && exit 0
+
+# Dump formatting diff and signal failure
+cat "$CLANG_FORMAT_OUTPUT"
+exit 1
